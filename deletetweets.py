35680e24e3e16656d996c1a8ce6c00f19b697653
Koen Rouwhorst <info@koenrouwhorst.nl>
2018-04-14 14:37:28 +0200

    Fixed a bunch of warnings.

--- deletetweets.py
+++ deletetweets.py
@@ -2,9 +2,9 @@
 
 import argparse
 import csv
-import twitter
-import time
 import sys
+import time
+import twitter
 from dateutil.parser import parse
 
 __author__ = "Koen Rouwhorst"
@@ -17,53 +17,58 @@ ACCESS_TOKEN = ""
 ACCESS_TOKEN_SECRET = ""
 
 def delete(api, date, r):
-  with open("tweets.csv") as file:
-    n = 0
+    with open("tweets.csv") as file:
+        count = 0
 
-    for row in csv.DictReader(file):
-      tweetId = int(row["tweet_id"])
-      tweetDate = parse(row["timestamp"], ignoretz=True).date()
+        for row in csv.DictReader(file):
+            tweet_id = int(row["tweet_id"])
+            tweet_date = parse(row["timestamp"], ignoretz=True).date()
 
-      if date != "" and tweetDate >= parse(date).date(): continue
+            if date != "" and tweet_date >= parse(date).date():
+                continue
 
-      if (r == "retweet" and row["retweeted_status_id"] == ""
-        or r == "reply" and row["in_reply_to_status_id"] == ""): continue
+            if (r == "retweet" and row["retweeted_status_id"] == "" or
+                    r == "reply" and row["in_reply_to_status_id"] == ""):
+                continue
 
-      try:
-        print "Deleting tweet #{0} ({1})".format(tweetId, tweetDate)
+            try:
+                print "Deleting tweet #{0} ({1})".format(tweet_id, tweet_date)
 
-        status = api.DestroyStatus(tweetId)
-        n += 1
-        time.sleep(1)
+                api.DestroyStatus(tweet_id)
+                count += 1
+                time.sleep(1)
 
-      except Exception, e:
-        print "Exception: %s\n" % e.message
+            except twitter.TwitterError, err:
+                print "Exception: %s\n" % err.message
 
-  print "Number of deleted tweets: %s\n" % n
+    print "Number of deleted tweets: %s\n" % count
 
-def error(msg, ec = 1):
+def error(msg, ec=1):
     sys.stderr.write("Error: %s\n" % msg)
     exit(ec)
 
 def main():
-  parser = argparse.ArgumentParser(description="Delete old tweets.")
-  parser.add_argument("-d", dest="date", required=True, help="Delete tweets until this date")
-  parser.add_argument("-r", dest="restrict", choices=["reply", "retweet"], help="Restrict to either replies or retweets")
+    parser = argparse.ArgumentParser(description="Delete old tweets.")
+    parser.add_argument("-d", dest="date", required=True,
+                        help="Delete tweets until this date")
+    parser.add_argument("-r", dest="restrict", choices=["reply", "retweet"],
+                        help="Restrict to either replies or retweets")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  if API_KEY == "" or API_SECRET =="":
-    error("No API key and/or secret set.")
+    if API_KEY == "" or API_SECRET == "":
+        error("No API key and/or secret set.")
 
-  if ACCESS_TOKEN == "" or ACCESS_TOKEN_SECRET =="":
-    error("No access token and/or secret set.")
+    if ACCESS_TOKEN == "" or ACCESS_TOKEN_SECRET == "":
+        error("No access token and/or secret set.")
 
-  api = twitter.Api(consumer_key=API_KEY,
-    consumer_secret=API_SECRET,
-    access_token_key=ACCESS_TOKEN,
-    access_token_secret=ACCESS_TOKEN_SECRET)
+    api = twitter.Api(consumer_key=API_KEY,
+                      consumer_secret=API_SECRET,
+                      access_token_key=ACCESS_TOKEN,
+                      access_token_secret=ACCESS_TOKEN_SECRET)
 
-  delete(api, args.date, args.restrict)
+    delete(api, args.date, args.restrict)
+
 
 if __name__ == "__main__":
-  main()
+    main()
