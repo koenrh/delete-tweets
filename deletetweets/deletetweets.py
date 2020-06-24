021f7f38076f8e92a725ba0a2a7c078b96f2807f
jorge <jsanchez@thesouthoracle.com>
2020-06-24 19:55:34 +0200

    Removed sleep in favour of sleep_on_rate_limit. This makes this tool blazing fast and more usable as a consequence.

--- deletetweets/deletetweets.py
+++ deletetweets/deletetweets.py
@@ -1,7 +1,6 @@
 import io
 import os
 import sys
-import time
 import json
 
 import twitter
@@ -19,17 +18,32 @@ class TweetDestroyer(object):
             print("delete tweet %s" % tweet_id)
             if not self.dry_run:
                 self.twitter_api.DestroyStatus(tweet_id)
-            # NOTE: A poor man's solution to honor Twitter's rate limits.
-            time.sleep(0.5)
         except twitter.TwitterError as err:
             print("Exception: %s\n" % err.message)
 
 
 class TweetReader(object):
-    def __init__(self, reader, since_date=None, until_date=None, filters=[], spare=[], min_likes=0, min_retweets=0):
+    def __init__(
+        self,
+        reader,
+        since_date=None,
+        until_date=None,
+        filters=[],
+        spare=[],
+        min_likes=0,
+        min_retweets=0,
+    ):
         self.reader = reader
-        self.since_date = datetime.min if since_date is None else parser.parse(since_date, ignoretz=True)
-        self.until_date = datetime.now() if until_date is None else parser.parse(until_date, ignoretz=True)
+        self.since_date = (
+            datetime.min
+            if since_date is None
+            else parser.parse(since_date, ignoretz=True)
+        )
+        self.until_date = (
+            datetime.now()
+            if until_date is None
+            else parser.parse(until_date, ignoretz=True)
+        )
         self.filters = filters
         self.spare = spare
         self.min_likes = 0 if min_likes is None else min_likes
@@ -42,34 +56,49 @@ class TweetReader(object):
                 if tweet_date >= self.until_date or tweet_date <= self.since_date:
                     continue
 
-            if ("retweets" in self.filters and
-                    not row["tweet"].get("full_text").startswith("RT @")) or \
-                    ("replies" in self.filters and
-                     row["tweet"].get("in_reply_to_user_id_str") == ""):
+            if (
+                "retweets" in self.filters
+                and not row["tweet"].get("full_text").startswith("RT @")
+            ) or (
+                "replies" in self.filters
+                and row["tweet"].get("in_reply_to_user_id_str") == ""
+            ):
                 continue
 
             if row["tweet"].get("id_str") in self.spare:
                 continue
 
-            if (self.min_likes > 0 and int(row["tweet"].get("favorite_count")) >= self.min_likes) or \
-                    (self.min_retweets > 0 and int(row["tweet"].get("retweet_count")) >= self.min_retweets):
+            if (
+                self.min_likes > 0
+                and int(row["tweet"].get("favorite_count")) >= self.min_likes
+            ) or (
+                self.min_retweets > 0
+                and int(row["tweet"].get("retweet_count")) >= self.min_retweets
+            ):
                 continue
 
             yield row
 
 
-def delete(tweetjs_path, since_date, until_date, filters, s, min_l, min_r, dry_run=False):
+def delete(
+    tweetjs_path, since_date, until_date, filters, s, min_l, min_r, dry_run=False
+):
     with io.open(tweetjs_path, mode="r", encoding="utf-8") as tweetjs_file:
         count = 0
 
-        api = twitter.Api(consumer_key=os.environ["TWITTER_CONSUMER_KEY"],
-                          consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"],
-                          access_token_key=os.environ["TWITTER_ACCESS_TOKEN"],
-                          access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"])
+        api = twitter.Api(
+            consumer_key=os.environ["TWITTER_CONSUMER_KEY"],
+            consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"],
+            access_token_key=os.environ["TWITTER_ACCESS_TOKEN"],
+            access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
+            sleep_on_rate_limit=True,
+        )
         destroyer = TweetDestroyer(api, dry_run)
 
         tweets = json.loads(tweetjs_file.read()[25:])
-        for row in TweetReader(tweets, since_date, until_date, filters, s, min_l, min_r).read():
+        for row in TweetReader(
+            tweets, since_date, until_date, filters, s, min_l, min_r
+        ).read():
             destroyer.destroy(row["tweet"]["id_str"])
             count += 1
 
